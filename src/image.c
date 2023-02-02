ae4a4bf0c43b91774ff35240549abc736911f777
TheLime03 <everpadd4@gmail.com>
2023-02-02 21:35:48 +0100

    l

---
+++ src/image.c
@@ -0,0 +1,36 @@
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include "image.h"
+
+Image *image_load(const char *filename)
+{
+    Image *image = (Image *)malloc(sizeof(Image));
+
+    SDL_Surface *surface = IMG_Load(filename);
+    if (!surface)
+    {
+        printf("error: %s\n", IMG_GetError());
+    }
+
+    image->width = surface->w;
+    image->height = surface->h;
+    image->pixels = (Uint32 *)surface->pixels;
+
+    return image;
+}
+
+void image_free(Image *image)
+{
+    free(image);
+}
+
+void image_draw(Image *image, int x, int y)
+{
+    // Draw the image on the screen
+    SDL_Rect rect;
+    rect.x = x;
+    rect.y = y;
+    rect.w = image->width;
+    rect.h = image->height;
+    SDL_BlitSurface((SDL_Surface *)image, NULL, screen, &rect);
+}
