f1c1f5f1142530baa53ed65c24643ed16ed232ac
TheLime03 <everpadd4@gmail.com>
2023-02-03 23:19:43 +0100

    updated the image_draw
    ++include h w peremetes

--- src/image.c
+++ src/image.c
@@ -4,34 +4,34 @@
 
 Image *image_load(const char *filename)
 {
-    Image *image = (Image *)malloc(sizeof(Image));
+    Image *image = (Image *)malloc(sizeof(Image)); // allocating memory for the image
 
-    SDL_Surface *surface = IMG_Load(filename);
+    SDL_Surface *surface = IMG_Load(filename); // loading the image from hard drive
     if (!surface)
     {
-        printf("error: %s\n", IMG_GetError());
+        printf("error: %s\n", IMG_GetError()); // error handling
     }
 
-    image->width = surface->w;
+    image->width = surface->w; // setting the width and height of the image
     image->height = surface->h;
-    image->pixels = (Uint32 *)surface->pixels;
+    image->pixels = (Uint32 *)surface->pixels; // setting the pixels of the image //TODO: check if this is correct
 
     return image;
 }
 
-void image_free(Image *image)
+void image_free(Image *image) // freeing the image from memory
 {
     free(image);
 }
 
-void image_draw(Image *image, int x, int y)
+void image_draw(Image *image, int x, int y, int w, int h)
 {
     SDL_Surface *screen;
     // Draw the image on the screen
-    SDL_Rect rect;
-    rect.x = x;
+    SDL_Rect rect; // creating a rectangle
+    rect.x = x;    // setting the x and y coordinates of the rectangle
     rect.y = y;
-    rect.w = image->width;
-    rect.h = image->height;
-    SDL_BlitSurface((SDL_Surface *)image, NULL, screen, &rect);
+    rect.w = w; // setting the width and height of the rectangle
+    rect.h = h;
+    SDL_BlitSurface((SDL_Surface *)image, NULL, screen, &rect); // drawing the image on the screen
 }
