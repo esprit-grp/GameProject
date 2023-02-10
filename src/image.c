2cf409c6c1219a116ea5dadc64dec8bc7232abc1
TheLime03 <everpadd4@gmail.com>
2023-02-10 15:09:10 +0100

    +text +FX +1 mouse motion event
    added text and sound effect
    +bug : :bug:  memory free bug

--- src/image.c
+++ src/image.c
@@ -70,7 +70,7 @@ void imageLoad_quitbutton(image *img)
     img->img_size.w = img->img->w;
     img->img_size.h = img->img->h;
     img->img_pos.x = ((SCREEN_W - img->img_size.w) / 2) - (img->img_size.w / 2);
-    img->img_pos.y = 400 * 2 + img->img_size.h * 2 + spacing * 2;
+    img->img_pos.y = 400 + img->img_size.h * 2 + spacing * 2;
 }
 
 // draw the images
@@ -94,7 +94,7 @@ void imageDraw_quitbutton(SDL_Surface *screen, image img)
 }
 
 // free the images
-void imageFree_background(image img)
+void imageFree(image img)
 {
     SDL_FreeSurface(img.img);
 }
