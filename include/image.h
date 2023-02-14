ab569d66c1eb5e5b4b6dc06d0b2b4d5f723534ac
Aymen Hmani <47940043+TheLime1@users.noreply.github.com>
2023-02-14 16:14:06 +0100

    adedd event for animations & pressing ESC to quit

--- include/image.h
+++ include/image.h
@@ -12,16 +12,28 @@ typedef struct
     SDL_Surface *img;  // pointer to the image
 } image;
 
+// load buttons assets
 void imageLoad_background(image *img);
 void imageLoad_playbutton(image *img);
 void imageLoad_settingsbutton(image *img);
 void imageLoad_quitbutton(image *img);
 
+void imageLoadClicked_playbutton(image *img);
+void imageLoadClicked_settingsbutton(image *img);
+void imageLoadClicked_quitbutton(image *img);
+
+// draw buttons
 void imageDraw_background(SDL_Surface *screen, image img);
 void imageDraw_playbutton(SDL_Surface *screen, image img);
 void imageDraw_settingsbutton(SDL_Surface *screen, image img);
 void imageDraw_quitbutton(SDL_Surface *screen, image img);
 
+// animate buttons
+void imageDrawClicked_playbutton(SDL_Surface *screen, image img);
+void imageDrawClicked_settingsbutton(SDL_Surface *screen, image img);
+void imageDrawClicked_quitbutton(SDL_Surface *screen, image img);
+
+// free buttons
 void imageFree(image img);
 
 #endif
