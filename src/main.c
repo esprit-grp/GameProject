c903540930888e4cf3bdd647d93c2f89d67eb1ad
MOHAsbissi01 <116494947+MOHAsbissi01@users.noreply.github.com>
2023-02-18 13:02:48 +0100

    new changes in what is related to music
    new changes in what is related to music

--- src/main.c
+++ src/main.c
@@ -8,20 +8,26 @@
 #include <SDL/SDL_mixer.h> //for loading sounds
 
 // including the headers
-#include "../include/image.h"
-#include "../include/music.h"
-#include "../include/text.h"
-#include "../include/display.h"
+#include "../include/menu.h"  //menu header
+#include "../include/music.h" //music header
+#include "../include/text.h"  //text header
 
-// images _C for clicked
+// screen
 SDL_Surface *screen;
+
+//* regular -> hovered -> clicked
+// images (_C for clicked) (_H for hovered)
 image background;
+image gametitle;
 image playButton;
-image playButton_C; 
+image playButton_C;
+image playButton_H;
 image settingsButton;
 image settingsButton_C;
+image settingsButton_H;
 image exitButton;
 image exitButton_C;
+image exitButton_H;
 
 // music
 Mix_Music *music;
@@ -32,7 +38,10 @@ text score;
 
 // logic
 SDL_Event event;
-int loop = 1;
+int loop = 1;             // game loop
+int anim_B = 0;           // animation for the buttons
+int isButtonAnimated = 0; // check if the button animated (to prevent FX spam)
+int selectedButton = 0;   // selected button (keyboard)
 
 /*
 ********************
@@ -48,27 +57,26 @@ int main()
         printf("SDL_Init Error: %s.\n", SDL_GetError());
         return 1;
     }
+     
+     //--------------------//
+     if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, 2, 1024) < 0) {
+        printf("Mix_OpenAudio failed: %s\n", Mix_GetError());
+        return 1;
+    }
+    //--------------------//
 
     // creating the window
-        screen = SDL_SetVideoMode(SCREEN_W, SCREEN_H, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-        if (screen == NULL)
-        {
-            printf("Error creating the screen: %s\n", SDL_GetError());
-            return 1;
-        }
-    // create the settings window
-        settings = SDL_SetVideoMode(SCREEN_W, SCREEN_H, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-        if (settings == NULL)
-        {
-            printf("Error creating the settings window: %s\n", SDL_GetError());
-            return 1;
-        }
-        SDL_WM_SetCaption("Settings", NULL);
-        SDL_Rect settings_rect = { 160, 120, 0, 0 };
+    screen = SDL_SetVideoMode(SCREEN_W, SCREEN_H, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
+    if (screen == NULL)
+    {
+        printf("Error creating the screen: %s\n", SDL_GetError());
+        return 1;
+    }
 
 
-    // loading background
+    // loading background and game title
     imageLoad_background(&background);
+    imageLoad_gametitle(&gametitle);
 
     // loading buttons
     imageLoad_playbutton(&playButton);
@@ -80,8 +88,17 @@ int main()
     imageLoadClicked_settingsbutton(&settingsButton_C);
     imageLoadClicked_quitbutton(&exitButton_C);
 
-    // loading music
-    musicLoad(music);
+    // loading hovered buttons
+    imageLoadHovered_playbutton(&playButton_H);
+    imageLoadHovered_settingsbutton(&settingsButton_H);
+    imageLoadHovered_quitbutton(&exitButton_H);
+     
+    //MUSIC
+        // loading music
+        musicLoad(music);
+        //playMusic
+        playMusic(music);
+     
 
     // loading text
     textLoad(&score);
@@ -94,17 +111,17 @@ int main()
 
     while (loop)
     {
-        // drawing images
+        // drawing background and game title
         imageDraw_background(screen, background);
+        imageDraw_gametitle(screen, gametitle);
+
+        // drawing intial buttons
         imageDraw_playbutton(screen, playButton);
         imageDraw_settingsbutton(screen, settingsButton);
         imageDraw_quitbutton(screen, exitButton);
 
         // drawing text
-        textDraw(screen, score, "Score: 0");
-
-        //handling events
-        SDL_Event event;
+        textDraw(screen, score, "KingsMan Team, 2023"); //? maybe add text ("by Kingsman team")
 
         // events
         while (SDL_PollEvent(&event))
@@ -112,58 +129,135 @@ int main()
             switch (event.type)
             {
                 case SDL_KEYDOWN:
-                        switch (event.key.keysym.sym)
-                        {  //shortcuts
-                        case SDLK_ESCAPE: //  presses ESC -> quit
-                            loop = 0;
-                            break;
-                        case SDLK_F : //  presses F -> fullscreen
-                            toggle_fullscreen(screen);
-                            break;
-                         case SDLK_s: //  presses s -> settings+newwindow
-                            SDL_BlitSurface(settings_window, NULL, main_window, &settings_rect);
-                            SDL_Flip(main_window);
-                            break;    
-                        default:
-                            break;
-                        }
-                        break;
+                            switch (event.key.keysym.sym)
+                            {
+                            case SDLK_ESCAPE:
+                                loop = 0;
+                                break;
+                            case SDLK_f:
+                                SDL_WM_ToggleFullScreen(screen); // TODO Move this to a button NOT A KEY PRESS
+                                break;
+
+                            // controlling the buttons by arrow keys
+                            // ! not stable we need to add an arrow or smth to show the selected button
+                            case SDLK_UP:
+                                selectedButton = (selectedButton - 1 + 3) % 3; // do the math here
+                                break;
+                            case SDLK_DOWN:
+                                selectedButton = (selectedButton + 1) % 3;
+                                break;
+                            case SDLK_RETURN:
+                                    switch (selectedButton)
+                                    {
+                                        case 0:
+                                            // start button pressed
+                                            imageDrawClicked_settingsbutton(screen, settingsButton_C);
+                                            FXLoad(clickFX);
+                                            break;
+                                        case 1:
+                                            // settings button pressed
+                                            imageDrawClicked_settingsbutton(screen, settingsButton_C);
+                                            FXLoad(clickFX);
+                                            break;
+                                        case 2:
+                                            // exit button pressed
+                                            imageDrawClicked_quitbutton(screen, exitButton_C);
+                                            FXLoad(clickFX);
+                                            break;
+                                    }
+                               //----------------//     
+                            case SDLK_PLUS:
+                                volume += 16;
+                                if(volume > 128)
+                                    volume = 128;
+                                Mix_VolumeMusic(volume);
+                                break;
+                            case SDLK_MINUS:
+                                volume -= 16;
+                                if(volume < 0)
+                                    volume = 0;
+                                Mix_VolumeMusic(volume);
+                                break;    
+                                //----------------//
+
+                            default:
+                                break;
+                            }
+                            break; // this break for preventing the mouse motion event from being triggered
 
                 case SDL_MOUSEMOTION:
-                        if (event.motion.x >= playButton.img_pos.x && event.motion.x <= playButton.img_pos.x + playButton.img_size.w && event.motion.y >= playButton.img_pos.y && event.motion.y <= playButton.img_pos.y + playButton.img_size.h) // if the mouse is on the play button
+                            if (event.motion.x >= playButton.img_pos.x && event.motion.x <= playButton.img_pos.x + playButton.img_size.w && event.motion.y >= playButton.img_pos.y && event.motion.y <= playButton.img_pos.y + playButton.img_size.h)
                             {
-                                FXLoad(clickFX);
-                                imageDrawClicked_playbutton(screen, playButton_C);
-                                SDL_Delay(1000);
+                                if (isButtonAnimated != 1)
+                                {
+                                    anim_B = 1;
+                                    FXLoad(clickFX);
+                                }
                             }
-                        {
-                            FXLoad(clickFX);
-                            imageDrawClicked_playbutton(screen, playButton_C);
-                            SDL_Delay(1000);
-                        }
-                        if (event.motion.x >= settingsButton.img_pos.x && event.motion.x <= settingsButton.img_pos.x + settingsButton.img_size.w && event.motion.y >= settingsButton.img_pos.y && event.motion.y <= settingsButton.img_pos.y + settingsButton.img_size.h) // if the mouse is on the settings button
-                        {
-                            imageDrawClicked_settingsbutton(screen, settingsButton_C);
-                            FXLoad(clickFX);
-                            SDL_Delay(1000);
-                        }
-                        if (event.motion.x >= exitButton.img_pos.x && event.motion.x <= exitButton.img_pos.x + exitButton.img_size.w && event.motion.y >= exitButton.img_pos.y && event.motion.y <= exitButton.img_pos.y + exitButton.img_size.h) // if the mouse is on the quit button
-                        {
-                            imageDrawClicked_quitbutton(screen, exitButton_C);
-                            FXLoad(clickFX);
-                            SDL_Delay(1000); 
-                        }
-                        break;
-                
+                            else if (event.motion.x >= settingsButton.img_pos.x && event.motion.x <= settingsButton.img_pos.x + settingsButton.img_size.w && event.motion.y >= settingsButton.img_pos.y && event.motion.y <= settingsButton.img_pos.y + settingsButton.img_size.h)
+                            {
+                                if (isButtonAnimated != 2)
+                                {
+                                    anim_B = 2;
+                                    FXLoad(clickFX);
+                                }
+                            }
+                            else if (event.motion.x >= exitButton.img_pos.x && event.motion.x <= exitButton.img_pos.x + exitButton.img_size.w && event.motion.y >= exitButton.img_pos.y && event.motion.y <= exitButton.img_pos.y + exitButton.img_size.h)
+                            {
+                                if (isButtonAnimated != 3)
+                                {
+                                    anim_B = 3;
+                                    FXLoad(clickFX);
+                                }
+                            }
+                            else
+                            {
+                                anim_B = 0;
+                            }
+                            break;
+                            //--------------------//
+                case SDL_MOUSEBUTTONDOWN :
+                            if (event.button.button == SDL_BUTTON_LEFT) {
+                                int x = event.button.x;
+                                int y = event.button.y;
+                                if (is_point_in_rect(x, y, increase_button)) {
+                                    volume += 10;
+                                    if(volume > 128)
+                                        volume = 128;
+                                    Mix_VolumeMusic(volume);
+                                } else if (is_point_in_rect(x, y, decrease_button)) {
+                                    volume -= 10;
+                                    if(volume < 0)
+                                        volume = 0;
+                                    Mix_VolumeMusic(volume);
+                                }
+                            }
+                            break;
+                       //--------------------//
                 case SDL_QUIT: // if the user clicks on the close button
-                        loop = 0;
-                        break;
-
-                          default:
-                        break;
+                            loop = 0;
+                            break;
+                        default:
+                            break;
             }
         }
 
+        // button animation logic
+        if (anim_B == 1)
+        {
+            imageDrawHovered_playbutton(screen, playButton_H);
+        }
+        else if (anim_B == 2)
+        {
+            imageDrawHovered_settingsbutton(screen, settingsButton_H);
+        }
+        else if (anim_B == 3)
+        {
+            imageDrawHovered_quitbutton(screen, exitButton_H);
+        }
+
+        // checking which button is animated
+        isButtonAnimated = anim_B;
         // rereshing the screen
         SDL_Flip(screen);
     }
@@ -172,10 +266,13 @@ int main()
     imageFree(background);
     imageFree(playButton);
     imageFree(playButton_C);
+    imageFree(playButton_H);
     imageFree(settingsButton);
     imageFree(settingsButton_C);
+    imageFree(settingsButton_H);
     imageFree(exitButton);
     imageFree(exitButton_C);
+    imageFree(exitButton_H);
     musicFree(music);
 
     SDL_Quit();
